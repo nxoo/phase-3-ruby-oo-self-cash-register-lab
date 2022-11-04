67af412870088ff5609330d2eb6df7e300d987f5
nxoo <nxoo.dev@gmail.com>
2022-11-04 14:17:57 +0300

    test passed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+  attr_accessor :items, :discount, :total, :last_transaction
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, amount, quantity = 1)
+    self.last_transaction = amount * quantity
+    self.total += self.last_transaction
+    quantity.times do
+      self.items << title
+    end
+  end
+
+  def apply_discount
+    if self.discount != 0
+      self.total = (total * ((100.0 -discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
+end
